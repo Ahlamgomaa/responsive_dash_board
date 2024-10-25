58938921d88a02128d4f9a20631016e87eee192e
Ahlam Gomaa <ahlam.gomaa@example.com>
2024-10-25 13:47:43 +0300

    user info list title part one

---
+++ lib/widgets/user_info_list_title.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class UserInfoListTitle extends StatelessWidget {
+  const UserInfoListTitle({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+  final String image, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(
+        image,
+      ),
+      title: Text(
+        title,
+        style: AppStyles.styleSemiBold16,
+      ),
+      subtitle: Text(
+        subtitle,
+        style: AppStyles.styleRegular12,
+      ),
+    );
+  }
+}
